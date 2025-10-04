494fe57f18378f890ff132def9d361d44bf33a4e
kouji <kawaikouji25@gmail.com>
2025-10-05 03:57:50 +0900

    アプリ毎に画面サイズ調整実装

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const BASE_REPOSITORY_NAME = 'portal'; // この統合サイトのリポジト�
 // ゲームごとの設定を管理するオブジェクト
 // 'default' は、個別の設定がないゲームに適用される
 const GAME_CONFIG = {
-    'janken': { width: '400px', height: '600px' }, // 例: じゃんけんゲームは縦長
+    'janken': { width: '720px', height: '1280px' }, // 例: じゃんけんゲームは縦長
     'game-puzzle': { width: '800px', height: '600px' }, // 例: パズルゲームは横長
     'default': { width: '100%', height: '100%' } // デフォルトはコンテナに合わせる
 };
